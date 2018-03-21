0d16643251085c30fe4f4cb618dcb1a8a7d1663b
Yargo AAOne <yargo+git-aao@jerq.org>
2018-03-21 22:31:40 +0000

    improve detection logic for netcat&socat, replace `expr` with `$(())` and cleanup formatting

--- nago.sh
+++ nago.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERID='nago.sh // 2010,2018-3-13 Yargo Bonetti // github.com/hb9kns/nago'
+VERID='nago.sh // 2010,2018-3-21 Yargo Bonetti // github.com/hb9kns/nago'
 
 # external programs
 ## text pager
@@ -9,19 +9,48 @@ browser=${BROWSER:-lynx -force_html}
 ## telnet client
 telnet=${TELNET:-telnet}
 ## image handler
-#imgviewer=display
-imgviewer=echo # dumb handler: only print image name
+#imgviewer () { display "$@" ; }
+# dumb handler: only print image name
+imgviewer () { echo '** image name:' "$@" ; }
+
+## find netcat or socat
+netcat=''
+if test "$NETCAT" = ""
+then for nn in netcat nc
+do if `$nn -v|grep destination 2>&1 >/dev/null`
+ then netcat=$nn
+  break
+ fi
+done
+else netcat="$NETCAT"
+fi
+if test "$netcat" = ""
+then if `socat -V|grep socat 2>&1 >/dev/null`
+ then socat=socat
+  echo "** using $socat" >&2
+ else socat=''
+  echo "** neither netcat nor socat found, will try snarf..." >&2
+ fi
+else echo "** using $netcat" >&2
+fi
 
 # netcat/socat/snarf fetcher -- this *must* work, or nago is dead!
 mync () {
-# if port<1 (i.e, local file) or missing, then 'cat `cat`'
  if test ${2:-0} -lt 1
+# if port<1 (i.e, local file) or missing
  then cat `cat`
- else
-# uncomment one one of the following and comment the others,
-# depending on what's available on your system
-  ${NETCAT:-/usr/pkg/bin/netcat} "$@" || socat -t9 tcp4:$1:$2 -
-#  read gf ; snarf "gopher://$1:$2/$gf" '-'
+# else try to fetch from remote
+ else if test "$netcat" != ""
+# if netcat executable
+  then $netcat -w 9 "$@"
+  else if test "$socat" != ""
+# if socat executable
+   then $socat -T9 tcp4:$1:$2 -
+# else try snarf
+   else read gf
+    snarf "gopher://$1:$2/$gf" '-'
+   fi
+  fi
  fi
 }
 
@@ -42,8 +71,8 @@ dirtmp=$tmpbase/$tmpprefix.$$.dir
 ftmp=$tmpbase/$tmpprefix.$$.tmp
 stack=$tmpbase/$tmpprefix.$$.stack
 
-if [ "$1" = "" ] ; then
- cat <<EOH
+if test "$1" = ""
+then cat <<EOH
  usage: $0 [-l <logfile>] [-f [<file>]|-h|<server>] [<directory> <port>]
  where <server> is a server in gopherspace, <directory> a subdir on it,
   and <port> the port to connect to (default $gopherport)
@@ -67,16 +96,16 @@ rmexit () { rm -f $dirtmp $ftmp $stack ; exit $1 ; }
 
 # make sure temporary files are writable
 rm -f $dirtmp $ftmp $stack
-if ! echo test > $dirtmp ; then
- echo "** cannot write into temporary file $dirtmp - giving up!"
+if ! echo test > $dirtmp
+then echo "** cannot write into temporary file $dirtmp - giving up!"
  rmexit 9
 fi
-if ! echo test > $ftmp ; then
- echo "** cannot write into temporary file $ftmp - giving up!"
+if ! echo test > $ftmp
+then echo "** cannot write into temporary file $ftmp - giving up!"
  rmexit 9
 fi
-if ! echo test > $stack ; then
- echo "** cannot write into stack file $stack - giving up!"
+if ! echo test > $stack
+then echo "** cannot write into stack file $stack - giving up!"
  rmexit 9
 fi
 
@@ -137,7 +166,7 @@ getdir () {
 # i=fake, don't generate a number, but remove leading 'i' from type
    i*) echo ".	$ft	$rest" >>$dirtmp ;;
 # otherwise it is an entry which may be selected: prepend number, increment
-   *) echo "  $ln	$ft	$rest" >>$dirtmp ; ln=`expr $ln + 1` ;;
+   *) echo "  $ln	$ft	$rest" >>$dirtmp ; ln=$(( $ln+1 )) ;;
    esac
   done
   }
@@ -188,7 +217,8 @@ selectdir () {
   a*) ln=${inp#?} ; act=$ACT_add ;;
   l*) ln=0 ; act=$ACT_log ;;
   *) ln=${inp:-0}
-     if [ $ln = 0 ] ; then act=$ACT_back
+     if test $ln = 0
+     then act=$ACT_back
      else act=$ACT_select
      fi ;;
   esac
@@ -201,7 +231,8 @@ selectdir () {
  *) s_typ=`grep "^  $ln	" $dirtmp | cut -f 2 | sed -e 's/\(.\).*/\1/'`
     s_por=`grep "^  $ln	" $dirtmp | cut -f 5`
 # if not enough fields, it's a top level address (server only)
-    if [ "$s_por" = "" ] ; then
+    if test "$s_por" = ""
+    then
      s_dir=/
      s_ser=`grep "^  $ln	" $dirtmp | cut -f 3`
      s_por=`grep "^  $ln	" $dirtmp | cut -f 4`
@@ -225,9 +256,11 @@ poplevel () {
  s_por=`tail -n 1 $stack | cut -f 2`
  s_dir=`tail -n 1 $stack | cut -f 3`
  ln=`wc $stack | { read l dummy ; echo $l ; }`
- if [ $ln -gt 0 ] ; then
-  ln=`expr $ln - 1`
-  if head -n $ln $stack >$ftmp 2>/dev/null ; then cat $ftmp >$stack
+ if test $ln -gt 0
+ then
+  ln=$(( $ln-1 ))
+  if head -n $ln $stack >$ftmp 2>/dev/null
+  then cat $ftmp >$stack
   else :>$stack
   fi
  else
@@ -243,9 +276,10 @@ rm -f $stack
 pushlevel
 
 s_act=X
-while [ $s_act != $ACT_quit ] ; do
- pushlevel
- if getdir ; then selectdir
+while test $s_act != $ACT_quit
+do pushlevel
+ if getdir
+ then selectdir
   case $s_act in
   $ACT_back) poplevel ; poplevel ;;
   $ACT_other) echo "** going to $s_ser : $s_por, fetching $s_dir" ;;
@@ -268,18 +302,19 @@ while [ $s_act != $ACT_quit ] ; do
        case $s_typ in
        0) $pager $ftmp ;;
        7) $pager $ftmp ;;
-       g) $imgviewer $ftmp ;;
+       g) imgviewer $ftmp ;;
        h) $browser $ftmp ;;
-       I) $imgviewer $ftmp ;;
+       I) imgviewer $ftmp ;;
        *) echo "** cannot display file type $s_typ, only save" ;;
        esac
        echo "** enter local filename to save (empty: no saving)"
        read inp
-       while [ "$inp" != "" ] ; do
-        if [ -f "$inp" ] ; then echo "** warning: $inp exists!"
+       while test "$inp" != ""
+       do if test -f "$inp"
+        then echo "** warning: $inp exists!"
         elif cat $ftmp >"$inp" ; then break
         else echo "** error: could not write file $inp!"
-        echo "** enter local filename to save (empty: no saving)"
+         echo "** enter local filename to save (empty: no saving)"
         fi
        read inp
        done
